2875c01f720820e7bbfcae7ee08ba73fb5ba949f
ht1131589588 <1131589588@qq.com>
2017-11-24 14:34:03 +0800

    change to express

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const controller = require('./controller');
+const express = require('express');
 
-const app = new Koa();
+const app = express();
 
-app.use(bodyParser());
-app.use(controller());
+app.all('*', (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+	if (req.method == 'OPTIONS') {
+	  	res.send(200);
+	} else {
+	    next();
+	}
+});
+
+app.get('/test',function (req,res){
+    res.jsonp({
+        serverIP: '127.0.0.1',
+        name:'乘风',
+        github:'https://github.com/Faithree',
+        sex:'男',
+        examTime:'2017-04-13',
+        province:'广东',
+        city:'广州',
+        country:'中国',
+        age:22,
+    })
+})
 
 //在端口3000监听
-app.listen(3001);
-console.log('app started at port 3001...');
+var server = app.listen(3001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+})
