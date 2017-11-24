8a496a68058e133f70cd3e27d35f28a870e3b002
ht1131589588 <1131589588@qq.com>
2017-11-24 22:13:27 +0800

    tijiao

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
+var cors = require('koa-cors');
 
 const app = new Koa();
-
+app.use(cors());
 app.use(bodyParser());
 app.use(controller());
 
