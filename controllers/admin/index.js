256093fadf15f0353844f142a013cb1d483a3324
ht1131589588 <1131589588@qq.com>
2017-12-06 11:44:59 +0800

    数据库连接

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,5 +1,5 @@
 import AddressComponent from "../../prototype/addressComponent";
-
+import AdminModel from '../../models/admin/admin'
 
 
 class Admin extends AddressComponent {
@@ -8,18 +8,40 @@ class Admin extends AddressComponent {
         this.login = this.login.bind(this)
     }
     async login(req,res,next) {
+        try {
+            AdminModel.findAll({
+                where: {
+                    name: 'Jike'
+                }
+            }).then(function(p) {
+                var _data = {
+                    code:0,
+                    data:JSON.parse(JSON.stringify(p))
+                }
+                res.jsonp(_data);
+            }).catch(function(err){
+                console.log('failed: ' + err);
+            });
+        } catch (error) {
+            console.log("失败");
+        }
         
-        res.jsonp({
-            code: '0',
-            name:'乘风',
-            github:'https://github.com/Faithree',
-            sex:'男',
-            examTime:'2017-04-13',
-            province:'广东',
-            city:'广州',
-            country:'中国',
-            age:22,
-        })
+        // console.log(`find ${admins.length} admins:`);
+        // for(let p of admins){
+        //     console.log(JSON.stringify(p));
+        // }
+        // res.jsonp({
+        //     code: '0',
+        //     name:'乘风',
+        //     github:'https://github.com/Faithree',
+        //     sex:'男',
+        //     examTime:'2017-04-13',
+        //     province:'广东',
+        //     city:'广州',
+        //     country:'中国',
+        //     age:22,
+        // })
+        // return;
     }
 }
 
